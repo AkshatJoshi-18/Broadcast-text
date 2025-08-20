1fd31da9c0f8e9467970e07fafdaa66640dc597d
AkshatJoshi-18 <akshatjoshi8630@google.com>
2025-08-20 18:59:50 +0530

    dummy text

--- entrypoint-mysql.sh
+++ entrypoint-mysql.sh
@@ -2,7 +2,7 @@
 # Allow only internal network traffic
 iptables -A OUTPUT -d 172.18.0.0/16 -j ACCEPT
 iptables -A OUTPUT -d 127.0.0.1/8 -j ACCEPT
-# iptables -A OUTPUT -j DROP
+iptables -A OUTPUT -j DROP
 
 # Hand back to MariaDB’s official entrypoint
 exec /usr/local/bin/docker-entrypoint.sh "$@"
